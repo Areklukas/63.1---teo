f541efd71d7c83844578eef7fe0e562a2a0ff901
Areklukas <56909985+Areklukas@users.noreply.github.com>
2021-02-24 08:50:21 +0100

    Add files via upload

---
+++ 63.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ifstream plik1;
+    plik1.open("ciagi.txt");
+    int liczba;
+    string s;
+
+    while(!plik1.eof())
+	{
+    	plik1>>liczba;
+    	s = to_string(liczba);
+    	string s1 = s.substr(0, s.length() / 2);
+    	string s2 = s.substr(s.length() / 2);
+    	if(s1==s2)
+		{
+        	cout<<liczba<<endl;
+    	}
+    }
+    return 0;
+}
+
